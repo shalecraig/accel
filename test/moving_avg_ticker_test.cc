5436c52665c4c889c3c03afbaeaa139c55d18614
shalecraig <shalecraig@gmail.com>
2014-01-10 15:32:49 -0500

    Added a simple test for one method in the moving_avg_ticker

---
+++ test/moving_avg_ticker_test.cc
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+
+#include "../src/moving_avg_ticker.h"
+
+// TEST(blahTest, blah1) {
+//     EXPECT_EQ(1, 1);
+// }
+
+// TEST(SquareRootTest, PositiveNos) {
+//     EXPECT_EQ (18.0, square_root (324.0));
+//     EXPECT_EQ (25.4, square_root (645.16));
+//     EXPECT_EQ (50.3321, square_root (2533.310224));
+// }
+
+TEST(MovingAvgTicker, InvalidInputValues) {
+
+    moving_avg_values *allocated = NULL;
+    int retval = allocate_moving_avg(1, 0, &allocated);
+    EXPECT_EQ(NULL, allocated);
+    EXPECT_EQ(MOVING_AVG_PARAM_ERROR, retval);
+    retval = allocate_moving_avg(0, 1, &allocated);
+    EXPECT_EQ(NULL, allocated);
+    EXPECT_EQ(MOVING_AVG_PARAM_ERROR, retval);
+
+    // Pass in a non-null pointer-to-pointer.
+    moving_avg_values *initial_non_null_value = (moving_avg_values *) 1;
+    allocated = initial_non_null_value;
+    retval = allocate_moving_avg(1, 1, &allocated);
+    EXPECT_EQ(initial_non_null_value, allocated);
+    EXPECT_EQ(MOVING_AVG_PARAM_ERROR, retval);
+    // TODO: find a way to test failing malloc/calloc.
+}
+
+TEST(MovingAvgTicker, ValigInputValuesAllocatesObjects) {
+    moving_avg_values *allocated = NULL;
+}
+
+int main (int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    int returnValue;
+
+    //Do whatever setup here you will need for your tests here
+    //
+    //
+
+    returnValue =  RUN_ALL_TESTS();
+
+    //Do Your teardown here if required
+    //
+    //
+
+    return returnValue;
+}
