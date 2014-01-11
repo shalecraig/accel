81aff77ff88bb752bb10bede53e7822ac31fb4b0
shalecraig <shalecraig@gmail.com>
2014-01-11 02:59:25 -0500

    Added beginning of accel end-to-end tests

---
+++ test/accel_test.cc
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+
+#include "../src/accel.h"
+
+const void * void_null = NULL;
+
+int main (int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    int returnValue;
+
+    //Do whatever setup here you will need for your tests here
+    //
+    //
+
+    returnValue =  RUN_ALL_TESTS();
+
+    //Do Your teardown here if required
+    //
+    //
+
+    return returnValue;
+}
