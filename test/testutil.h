67c087fec6f206ba2c33d243591431829399f41a
shalecraig <shalecraig@gmail.com>
2014-03-12 23:16:57 -0700

    Updated the testutil header to work.
    TODO: why didn't the prev one work?

--- test/testutil.h
+++ test/testutil.h
@@ -1,24 +1,20 @@
 #ifndef TEST_CALLBACK_UTIL_H
 #define TEST_CALLBACK_UTIL_H
 
-// TODO: build a list of params
-// TODO: build a list of returned values.
-
-#define TEST_CALLBACK_NAMESPACE(testClass, testName, uniqueIdentifier) \
-    generated_##testClass##_##testName##_##uniqueIdentifier##_
+// TODO: this is so hack.
 
 #define TEST_CALLBACK_NAME(testClass, testName, uniqueIdentifier) \
-    TEST_CALLBACK_NAMESPACE(testClass, testName, uniqueIdentifier)
+    generated_##testClass##_##testName##_##uniqueIdentifier##_name
 
 #define TEST_CALLBACK_COUNTER(testClass, testName, uniqueIdentifier) \
     generated_##testClass##_##testName##_##uniqueIdentifier##_counter
 
 #define TEST_CALLBACK(returnSignature, testClass, testName, uniqueIdentifier, args...) \
     /* count the number of times the callback was invoked. */ \
-    uint TEST_CALLBACK_COUNTER(testClass, testName, uniqueIdentifier) = 0; \
+    uint generated_##testClass##_##testName##_##uniqueIdentifier##_counter = 0; \
     /* define a method that proxies calls to the real method. */ \
-    returnSignature TEST_CALLBACK_NAME(testClass, testName, uniqueIdentifier) (args) { \
+    returnSignature generated_##testClass##_##testName##_##uniqueIdentifier##_name (args) { \
         /* increment the counter */ \
-        TEST_CALLBACK_COUNTER(testClass, testName, uniqueIdentifier) += 1;
+        generated_##testClass##_##testName##_##uniqueIdentifier##_counter += 1;
 
 #endif
