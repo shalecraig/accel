d6a6c6882b7edb3380afb2ceceeff2b9cad1371a
shalecraig <shalecraig@gmail.com>
2014-03-18 00:45:53 -0700

    added comment describing the change

--- s/hooks/auto/pre-commit/clang-format.sh
+++ s/hooks/auto/pre-commit/clang-format.sh
@@ -3,6 +3,7 @@ set -e
 
 pushd $(git rev-parse --show-toplevel || echo ".")
 
+# Use clang-format on changed files.
 git diff --name-only --cached | \
     grep -E '\.(h|c|cc|cpp)' | \
     xargs clang-format -i
